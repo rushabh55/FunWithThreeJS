d34212b2cd7ae16ecd4bac8a406f00b166b1e43c
Rush <rushabh.techie@gmaail.com>
2015-03-15 20:49:30 -0400

    funny moon orbit.

--- js/Constants.js
+++ js/Constants.js
@@ -6,3 +6,13 @@
            'Textures/skybox/zp.png',
            'Textures/skybox/zn.png'
 ];
+var sphereRadius = 10000;
+var earthRadiusFactor = 0.025;
+var moonRadiusFactor = 0.005;
+var sunRadiusFactor = 1;
+var sphereQuality = 50;
+var pointLightRange = 10000000;
+var FARPLANE = 100000000;
+var ROTATIONSPEED = 10;
+var sun2EarthDist = -25000;
+var maxStars = 10000;
